bf7313e8995fe382cefa73f255153aa18a5ef923
stutii-15 <stutibastwade@gmail.com>
2025-09-29 14:42:54 +0530

    Update SpeedController.java

--- LAB-4/SpeedController.java
+++ LAB-4/SpeedController.java
@@ -1,9 +1,9 @@
 public class SpeedController {
 
-    // Method to check speed
+    
     public void checkSpeed(int speed) {
         if (speed > 100) {
-            // Throw an exception if speed exceeds limit
+           
             throw new ArithmeticException("Speed limit exceeded!");
         } else {
             System.out.println("Speed is normal.");
@@ -14,15 +14,16 @@ public class SpeedController {
         SpeedController controller = new SpeedController();
 
         try {
-            // Safe speed
+          
             controller.checkSpeed(80);
 
-            // Unsafe speed
+         
             controller.checkSpeed(120);
 
         } catch (ArithmeticException e) {
-            // Catch the thrown exception
+           
             System.out.println("Error: " + e.getMessage());
         }
     }
 }
+
